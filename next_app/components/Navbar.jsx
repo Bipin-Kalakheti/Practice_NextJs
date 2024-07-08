e27b6490582b7372d789322b68a72dc249ec0d71
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-07-07 23:59:28 -0400

    chore: Update layout and add Navbar component

---
+++ next_app/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Link from "next/link";
+const Links = [
+  { href: "/counter", text: "Counter" },
+  { href: "/bar", text: "Bar" },
+  { href: "/todo-list", text: "Todo List" },
+  { href: "/query", text: "Query" },
+];
+
+const Navbar = () => {
+  return (
+    <nav className="bg-base-300">
+      <div className="navbar max-w-6xl mx-auto flex-col sm:flex-row">
+        <Link href="/" className="btn btn-primary">
+          Home
+        </Link>
+        <ul className="menu md:ml-8 flex-col sm:flex-row">
+          {Links.map((link) => (
+            <li key={link.href}>
+              <Link href={link.href} className="">
+                {link.text}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
