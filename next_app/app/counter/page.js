f1c026212f1db257d6968ffbae32e4f642a5458e
Bipin_Kalakheti <100770518+Bipin-Kalakheti@users.noreply.github.com>
2024-07-11 14:05:22 -0400

    Refactor Counter component to use useState hook for managing count state

--- next_app/app/counter/page.js
+++ next_app/app/counter/page.js
@@ -1,7 +1,29 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const Counter = () => {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-7xl font-bold mb-4">{count}</h1>
+      <button
+        className="btn btn-primary m-2"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </button>
+      <button className="btn btn-secondary m-2" onClick={() => setCount(0)}>
+        Reset
+      </button>
+      <button
+        className="btn btn-primary m-2"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrement
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
